ad70f248d06bffb3da02d8b910f7017c70c5b285
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-04-10 16:58:52 -0400

    project created

---
+++ src/main/java/com/katebayard/cookshelf/CookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.katebayard.cookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookshelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookshelfApplication.class, args);
+    }
+
+}
